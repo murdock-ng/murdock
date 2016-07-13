c194aea4053210f14ed1dc7ba7059a9d90f50d48
Kaspar Schleiser <kaspar@schleiser.de>
2016-07-13 11:18:14 +0200

    adapt build.sh to job command actions

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
-#!/bin/sh -xe
+#!/bin/sh -ex
 
-set
+ACTION="$1"
 
-. "$CI_SCRIPTS_DIR/build_local.sh"
+case "$ACTION" in
+    build)
+        . "$CI_SCRIPTS_DIR/build_local.sh"
 
-echo "PWD: $(pwd)"
-echo "Building PR#$CI_PULL_NR $CI_PULL_URL head: $CI_PULL_COMMIT..."
+        echo "Building PR#$CI_PULL_NR $CI_PULL_URL head: $CI_PULL_COMMIT..."
 
-git clone $CI_BASE_REPO -b $CI_BASE_BRANCH build
-cd build
-if [ "$CI_BASE_REPO" != "$CI_PULL_REPO" ]; then
-    git fetch $CI_PULL_REPO
-fi
+        git clone $CI_BASE_REPO -b $CI_BASE_BRANCH build
 
-git reset --hard $CI_PULL_COMMIT
-git rebase $CI_BASE_BRANCH
+        cd build
+        if [ "$CI_BASE_REPO" != "$CI_PULL_REPO" ]; then
+            git fetch origin pull/$CI_PULL_NR/head
+        fi
 
-time build
+        git checkout $CI_PULL_COMMIT
+        git rebase $CI_BASE_BRANCH
+
+        build || exit 1
+        ;;
+    post_build)
+        cat output.txt | ansi2html -s solarized -u > output.html
+        ;;
+    *)
+        echo "$0: unhandled action $ACTION"
+        exit 1
+esac
