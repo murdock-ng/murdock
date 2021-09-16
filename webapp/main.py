5aed15fdcbb833fdb233864fc5c60e1049245cdb
Alexandre Abadie <alexandre.abadie@inria.fr>
2021-09-16 16:23:57 +0200

    webapp: add basic application for serving the frontend app

---
+++ webapp/main.py
@@ -0,0 +1,10 @@
+from starlette.applications import Starlette
+from starlette.routing import Mount
+from starlette.staticfiles import StaticFiles
+
+
+routes = [
+    Mount('/', app=StaticFiles(directory='webapp/build', html=True), name="app"),
+]
+
+app = Starlette(routes=routes)
