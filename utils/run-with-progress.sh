24b7da15e3e240e8564a74f91e537be14c22f436
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-02-14 08:50:47 +0100

    utils: adapt scripts to new job workflow

--- utils/run-with-progress.sh
+++ utils/run-with-progress.sh
@@ -9,71 +9,60 @@
 # To use this script, set MURDOCK_SCRIPT_NAME=run-with-progress.sh in a .env
 # file or to the server command line.
 
+: ${COLLECTING_JOBS_DELAY:=3}
+: ${NUM_JOBS:=10}
+: ${JOB_DELAY:=1}
+: ${POST_BUILD_DELAY:=3}
 
-ACTION="$1"
+main() {
+    echo "-- Working directory: $(pwd)"
+    echo "-- Environment vars:"
+    env
 
-COLLECTING_JOBS_DELAY=1
-NUM_JOBS=5
-JOB_DELAY=1
-POST_BUILD_DELAY=1
+    local start_time=$(date +%s)
+    echo "--- Start build"
 
-case "$ACTION" in
-    run)
-        echo "-- Working directory: $(pwd)"
-        echo "-- Environment vars:"
-        env
+    echo "--- Collecting jobs"
+    local status='{"status" : {"status": "collecting jobs"}}'
+    /usr/bin/curl -s -d "${status}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT ${CI_BASE_URL}/jobs/running/${CI_JOB_UID}/status > /dev/null
+    sleep ${COLLECTING_JOBS_DELAY}
 
-        START_TIME=$(date +%s)
-        echo "--- Start build"
+    echo "--- Running jobs"
 
-        echo "--- Collecting jobs"
-        STATUS='{"status" : {"status": "collecting jobs"}}'
-        /usr/bin/curl -d "${STATUS}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT ${CI_BASE_URL}/jobs/running/${CI_JOB_UID}/status > /dev/null
-        sleep ${COLLECTING_JOBS_DELAY}
-
-        echo "--- Running jobs"
-
-        RETVAL=$(($RANDOM % 2))
-        CANCELED=$(($RANDOM % 2))
-        FAILED_BUILDS=""
-        for job in $(seq 0 1 ${NUM_JOBS})
-        do
-            echo "---- Job ${job} started"
-            echo "I am job ${job} (ret: ${RETVAL})" > job_${job}.txt
-            if [ "${RETVAL}" -eq 1 ]
-            then
-                if  [ "${job}" -ne 0 ]
-                then
-                    FAILED_BUILDS+=", "
-                fi
-                FAILED_BUILDS+='{"name": "job_'${job}'", "href": "'${CI_BASE_URL}'/results/'${CI_JOB_UID}'/job_'${job}'.txt"}'
-                STATUS='{"status" : {"status": "working", "total": '${NUM_JOBS}', "failed": '${job}', "passed": 0, "eta": "'$((${NUM_JOBS} - ${job}))'", "failed_builds": ['${FAILED_BUILDS}']}}'
-            else
-                STATUS='{"status" : {"status": "working", "total": '${NUM_JOBS}', "failed": 0, "passed": '${job}', "eta": "'$((${NUM_JOBS} - ${job}))'"}}'
-            fi
-            /usr/bin/curl -d "${STATUS}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT ${CI_BASE_URL}/jobs/running/${CI_JOB_UID}/status > /dev/null
-            sleep ${JOB_DELAY}
-            echo "---- Job ${job} finished"
-            echo
-        done
-
-        if [ "${RETVAL}" -eq 1 ] && [ "${CANCELED}" -eq 1 ]
+    local retval=$(($RANDOM % 2))
+    local canceled=$(($RANDOM % 2))
+    local failed_builds=""
+    for job in $(seq 0 1 ${NUM_JOBS})
+    do
+        echo "---- Job ${job} started"
+        echo "I am job ${job} (ret: ${retval})" > job_${job}.txt
+        if [ "${retval}" -eq 1 ]
         then
-            STATUS='{"status" : {"status": "canceled", "failed_builds": ['${FAILED_BUILDS}']}}'
-            /usr/bin/curl -d "${STATUS}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT ${CI_BASE_URL}/jobs/running/${CI_JOB_UID}/status > /dev/null
+            if  [ "${job}" -ne 0 ]
+            then
+                failed_builds+=", "
+            fi
+            failed_builds+='{"name": "job_'${job}'", "href": "'${CI_BASE_URL}'/results/'${CI_JOB_UID}'/job_'${job}'.txt"}'
+            status='{"status" : {"status": "working", "total": '${NUM_JOBS}', "failed": '${job}', "passed": 0, "eta": "'$((${NUM_JOBS} - ${job}))'", "failed_builds": ['${failed_builds}']}}'
+        else
+            status='{"status" : {"status": "working", "total": '${NUM_JOBS}', "failed": 0, "passed": '${job}', "eta": "'$((${NUM_JOBS} - ${job}))'"}}'
         fi
+        /usr/bin/curl -s -d "${status}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT ${CI_BASE_URL}/jobs/running/${CI_JOB_UID}/status > /dev/null
+        sleep ${JOB_DELAY}
+        echo "---- Job ${job} finished"
+        echo
+    done
 
-        echo "--- Build completed (elapsed: $(($(date +%s) - ${START_TIME}))s, ret=${RETVAL})"
-        exit $RETVAL
-        ;;
-    finalize)
-        echo "-- Post build action"
-        sleep ${POST_BUILD_DELAY}
-        cat output.txt | /usr/bin/ansi2html > output.html
-        exit 0
-        ;;
-    *)
-        echo "$0: unhandled action $ACTION"
-        exit 1
-        ;;
-esac
+    if [ "${retval}" -eq 1 ] && [ "${canceled}" -eq 1 ]
+    then
+        status='{"status" : {"status": "canceled", "failed_builds": ['${failed_builds}']}}'
+        /usr/bin/curl -s -d "${status}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT ${CI_BASE_URL}/jobs/running/${CI_JOB_UID}/status > /dev/null
+    fi
+
+    echo "--- Build completed (elapsed: $(($(date +%s) - ${start_time}))s, ret=${retval})"
+    echo "-- Finalize output"
+    sleep ${POST_BUILD_DELAY}
+    exit ${retval}
+}
+
+main
--- utils/run.sh
+++ utils/run.sh
@@ -2,49 +2,38 @@
 
 # This script is the default and basic run.sh example.
 
-ACTION="$1"
-
 COLLECTING_JOBS_DELAY=1
 NUM_JOBS=5
 JOB_DELAY=1
 POST_BUILD_DELAY=1
 
-case "$ACTION" in
-    run)
-        echo "-- Working directory: $(pwd)"
-        echo "-- Environment vars:"
-        env
+main() {
+    echo "-- Working directory: $(pwd)"
+    echo "-- Environment vars:"
+    env
 
-        START_TIME=$(date +%s)
-        echo "--- Start build"
+    local start_time=$(date +%s)
+    echo "--- Start build"
 
-        echo "--- Collecting jobs"
-        sleep ${COLLECTING_JOBS_DELAY}
+    echo "--- Collecting jobs"
+    sleep ${COLLECTING_JOBS_DELAY}
 
-        echo "--- Running jobs"
+    echo "--- Running jobs"
 
-        RETVAL=$(($RANDOM % 2))
-        CANCELED=$(($RANDOM % 2))
-        FAILED_BUILDS=""
-        for job in $(seq 0 1 ${NUM_JOBS})
-        do
-            echo "---- Job ${job} started "
-            sleep ${JOB_DELAY}
-            echo "---- Job ${job} finished "
-            echo
-        done
+    local retval=$(($RANDOM % 2))
+    for job in $(seq 0 1 ${NUM_JOBS})
+    do
+        echo "---- Job ${job} started "
+        sleep ${JOB_DELAY}
+        echo "---- Job ${job} finished "
+        echo
+    done
 
-        echo "--- Build completed (elapsed: $(($(date +%s) - ${START_TIME}))s, ret=${RETVAL})"
-        exit $RETVAL
-        ;;
-    finalize)
-        echo "-- Post build action"
-        sleep ${POST_BUILD_DELAY}
-        cat output.txt | /usr/bin/ansi2html > output.html
-        exit 0
-        ;;
-    *)
-        echo "$0: unhandled action $ACTION"
-        exit 1
-        ;;
-esac
+    echo "--- Build completed (elapsed: $(($(date +%s) - ${start_time}))s, ret=${retval})"
+
+    echo "-- Finalize"
+    sleep ${POST_BUILD_DELAY}
+    exit ${retval}
+}
+
+main
